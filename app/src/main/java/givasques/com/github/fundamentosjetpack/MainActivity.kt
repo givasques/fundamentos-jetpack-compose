d66cf86ca51950086a72d53ba04257fbb6bee8a8
Giovanna <giovanna.vasques1101@hotmail.com>
2025-08-24 15:37:36 -0300

    Box

--- app/src/main/java/givasques/com/github/fundamentosjetpack/MainActivity.kt
+++ app/src/main/java/givasques/com/github/fundamentosjetpack/MainActivity.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import givasques.com.github.fundamentosjetpack.ui.theme.FundamentosjetpackTheme
 
 
@@ -24,7 +31,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             FundamentosjetpackTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Inicial(Modifier.padding(innerPadding))
+                    NotificationBage(Modifier.padding(innerPadding))
                 }
             }
         }
@@ -50,11 +57,39 @@ class MainActivity : ComponentActivity() {
         )
     }
 
+    @Composable
+    fun NotificationBage(modifier: Modifier = Modifier) {
+        Box(modifier = modifier.padding(16.dp), contentAlignment = Alignment.Center) {
+            Box(
+                modifier = Modifier
+                    .size(24.dp)
+                    .clip(CircleShape)
+                    .background(color = Color.Green)
+            )
+            Box(
+                modifier = Modifier
+                    .size(8.dp)
+                    .clip(CircleShape)
+                    .background(color = Color.Red)
+                    .align(Alignment.TopEnd)
+            )
+        }
+
+    }
+
     @Preview(showBackground = true)
     @Composable
     fun PreviewFuncaoInicial() {
         FundamentosjetpackTheme {
             Inicial()
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun PreviewNotificationBage() {
+        FundamentosjetpackTheme {
+            NotificationBage()
         }
     }
 }
