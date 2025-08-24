c7c91e54aaa56d62f8ec28fb9f381216a5b292ac
Giovanna <giovanna.vasques1101@hotmail.com>
2025-08-24 15:17:46 -0300

    Adicionando mensagem de hello world na MainActivity.kt

--- app/src/main/java/givasques/com/github/fundamentosjetpack/MainActivity.kt
+++ app/src/main/java/givasques/com/github/fundamentosjetpack/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.tooling.preview.Preview
 import givasques.com.github.fundamentosjetpack.ui.theme.FundamentosjetpackTheme
 
@@ -20,28 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             FundamentosjetpackTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Text (text = "Hello World", modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FundamentosjetpackTheme {
-        Greeting("Android")
     }
 }
