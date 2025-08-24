9738a5b357aee5efd57a4dae155d12ad35ef4017
Giovanna <giovanna.vasques1101@hotmail.com>
2025-08-24 15:34:24 -0300

    Column e Row

--- app/src/main/java/givasques/com/github/fundamentosjetpack/MainActivity.kt
+++ app/src/main/java/givasques/com/github/fundamentosjetpack/MainActivity.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
-import androidx.compose.ui.modifier.ModifierLocalReadScope
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import givasques.com.github.fundamentosjetpack.ui.theme.FundamentosjetpackTheme
 
@@ -31,13 +32,20 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun Inicial(modifier: Modifier = Modifier) {
-        TextoCustomizado("Primeira função composable", modifier)
+        Column (modifier = modifier.background(color=Color.Blue)){
+            TextoCustomizado("Primeira função composable")
+            Row {
+                TextoCustomizado("Seja bem vindo")
+                TextoCustomizado("Olá usuário")
+            }
+        }
     }
 
     @Composable
     fun TextoCustomizado (texto: String, modifier: Modifier = Modifier) {
         Text (
             text = texto,
+            color = Color.White,
             modifier = modifier
         )
     }
